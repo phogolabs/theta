4369c0e87d5c3699d93adbdbd71414d79b2cbaae
Svetlin Ralchev <svet@ralch.com>
2020-01-29 13:14:42 +0000

    Add WithStore option

--- kinesis_store.go
+++ kinesis_store.go
@@ -8,6 +8,11 @@ import (
 // KinesisCollectorStore represents a kinesis collector store
 type KinesisCollectorStore = consumer.Store
 
+// KinesisCollectorWithStore creates a store option
+func KinesisCollectorWithStore(store KinesisCollectorStore) KinesisCollectorOption {
+	return consumer.WithStore(store)
+}
+
 // NewKinesisCollectorPSQLStore creates a new PostgreSQL store
 func NewKinesisCollectorPSQLStore(name, table string, url string) (KinesisCollectorStore, error) {
 	return psql.New(name, table, url)
